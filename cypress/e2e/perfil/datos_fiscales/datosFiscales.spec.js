da56e75cfce88ee2866f3a1a77ef9ac45b671770
Jesús González <jesusmanuel.gonzalez@axity.com>
2025-09-12 18:16:09 -0600

    Se refactorizo las pruebas de datos fiscales

---
+++ cypress/e2e/perfil/datos_fiscales/datosFiscales.spec.js
@@ -0,0 +1,57 @@
+import LoginPage from '../../login/loginPage';
+import PerfilPage from '../perfilPage';
+import DatosFiscalesPage from './datosFiscalesPage';
+
+describe("Datos fiscales", () => {
+  const perfil = new PerfilPage();
+  const datosFiscales = new DatosFiscalesPage();
+  const log = new LoginPage();
+
+  // Usar variables de nivel suite
+  let loginData;
+  let datosFiscalesData;
+
+  before(() => {
+    cy.fixture("data_general").then((data) => {
+      loginData = data;
+    });
+    cy.fixture("infoDatos_fiscales/data_Fiscales").then((data) => {
+      datosFiscalesData = data;
+    });
+  });
+
+  beforeEach(() => {
+    // Validar que estén cargados antes de usar
+    expect(loginData, "login data cargado").to.not.be.undefined;
+    expect(datosFiscalesData, "datos fiscales cargado").to.not.be.undefined;
+
+    log.login(loginData.url, loginData.correo, loginData.contrasena);
+    perfil.botonPerfil();
+    perfil.botonDatosFiscales();
+  });
+
+  it("Agregar nuevos datos fiscales @regression", () => {
+    datosFiscales.botonAgregarDatosFiscales();
+    datosFiscales.llenarDatosFiscales(
+      datosFiscalesData.razonSocial,
+      datosFiscalesData.rfc,
+      datosFiscalesData.calle,
+      datosFiscalesData.correoFiscal
+    );
+    datosFiscales.botonGuardarDatos();
+    datosFiscales.validarMensajeExito('Los datos fiscales se agregaron con éxito');
+  });
+
+  it("Editar datos fiscales @regression", () => {
+    datosFiscales.botonEditarDatosFiscales(datosFiscalesData.razonSocial);
+    datosFiscales.editarDatosFiscales();
+    datosFiscales.botonGuardarDatos();
+    datosFiscales.validarMensajeExito('Los datos fiscales se agregaron con éxito');
+  });
+
+  it("Eliminar datos fiscales @regression", () => {
+    datosFiscales.eliminarDatosFiscales(datosFiscalesData.razonSocial);
+    perfil.botonAceptar();
+    datosFiscales.validarMensajeExito('Los datos fiscales fueron eliminados con éxito');
+  });
+});
